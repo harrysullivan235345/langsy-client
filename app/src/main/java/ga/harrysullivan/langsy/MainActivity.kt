148c9fa42d96eb77df796eea3854b5ef75af2daa
Harry Sullivan <aditya.michigan@gmail.com>
2020-01-22 19:11:32 -0500

    inital commit

---
+++ app/src/main/java/ga/harrysullivan/langsy/MainActivity.kt
@@ -0,0 +1,22 @@
+package ga.harrysullivan.langsy
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.MotionEvent
+import android.view.View.OnTouchListener
+import androidx.core.app.ComponentActivity
+import androidx.core.app.ComponentActivity.ExtraData
+import androidx.core.content.ContextCompat.getSystemService
+import android.icu.lang.UCharacter.GraphemeClusterBreak.T
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
