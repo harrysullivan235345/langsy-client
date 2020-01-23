e0aeb518d554e95c277b57007b79f2f461d5f20b
Harry Sullivan <aditya.michigan@gmail.com>
2020-01-22 20:02:03 -0500

    first part of dashboard
    First part of dashboard

---
+++ app/src/main/java/ga/harrysullivan/langsy/CourseLabel.kt
@@ -0,0 +1,12 @@
+package ga.harrysullivan.langsy
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CourseLabel : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_course_label)
+    }
+}
---
+++ app/src/main/java/ga/harrysullivan/langsy/DashboardActivity.kt
@@ -0,0 +1,14 @@
+package ga.harrysullivan.langsy
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.LayoutInflater
+
+class DashboardActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dashboard)
+
+    }
+}
