e9075c49a1b9d0302ae861ccfed3ea9fa27fc36e
Harry Sullivan <aditya.michigan@gmail.com>
2020-01-25 20:35:48 -0500

    application ui

---
+++ app/src/main/java/ga/harrysullivan/langsy/ApplicationLearningActivity.kt
@@ -0,0 +1,12 @@
+package ga.harrysullivan.langsy
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ApplicationLearningActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_application_learning)
+    }
+}
--- app/src/main/java/ga/harrysullivan/langsy/VisualLearningActivity.kt
+++ app/src/main/java/ga/harrysullivan/langsy/VisualLearningActivity.kt
@@ -1,12 +1,19 @@
 package ga.harrysullivan.langsy
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_visual_learning.*
 
 class VisualLearningActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_visual_learning)
+
+        visual_learning_next_button.setOnClickListener {
+            val intent = Intent(this@VisualLearningActivity, ApplicationLearningActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
