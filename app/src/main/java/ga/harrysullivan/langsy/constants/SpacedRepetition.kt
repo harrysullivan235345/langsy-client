5dc8667421b0e8d23612793ca834597ca18e83a0
Harry Sullivan <aditya.michigan@gmail.com>
2020-01-26 19:23:56 -0500

    Spaced repetition constants

---
+++ app/src/main/java/ga/harrysullivan/langsy/constants/SpacedRepetition.kt
@@ -0,0 +1,6 @@
+package ga.harrysullivan.langsy.constants
+
+object SpacedRepetition {
+    val STAGES = intArrayOf(25, 2*60, 10*60, 60*60, 5*60*60, 25*60*60, 4*30*60*60)
+    val WORKING_MEMORY_CAPACITY = 5
+}
