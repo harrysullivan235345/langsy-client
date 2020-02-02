9ae325884243a408a9f273cc58335ceababd83e7
Harry Sullivan <aditya.michigan@gmail.com>
2020-02-02 12:08:04 -0500

    normalization

--- app/src/main/java/ga/harrysullivan/langsy/utils/Validation.kt
+++ app/src/main/java/ga/harrysullivan/langsy/utils/Validation.kt
@@ -2,12 +2,14 @@ package ga.harrysullivan.langsy.utils
 
 object Validation {
     fun validate(s1: String, s2: String): Boolean {
-        val notEmpty = s1.isNotEmpty()
+        val notEmpty = normalize(s1).isNotEmpty()
         val hasTargetWord = has(s1, s2)
         return notEmpty && hasTargetWord
     }
 
+    private val normalize = { s: String -> s.toLowerCase().trim() }
+
     private fun has(s1: String, s2: String): Boolean {
-        return s1.indexOf(s2) != -1
+        return normalize(s1).indexOf(normalize(s2)) != -1
     }
 }
